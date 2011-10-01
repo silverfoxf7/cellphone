7a71d6e35ec4cd311d6b01ab41a61fc4b9326e84
Rodrigo <joserodrigofuentes@gmail.com>
2011-10-01 11:12:22 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellphone::Application.config.secret_token = '88cb6da9bf7761594466df1935b81cea10fcdea36b2c9f2507c70729f0c29c1ebe1a11c74ecc17a19548d474459a1abb0492143e7e562a802a5465504b8d90cf'
